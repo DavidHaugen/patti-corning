fa491900f330b55792dbe468d0b6302f69cf7b55
David Haugen <haugen.dj@gmail.com>
2019-07-22 16:56:47 -0500

    add sample image box and sample image files

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import hibiscus from '../images/hibiscus.jpeg';
+import paintingFlowers from '../images/painting-flowers.jpeg';
+import tulip from '../images/tulip.jpeg';
+import './Home.css';
 
 export default class Home extends Component{
   render(){
     return (
-      <>
-
-      </>
+      <div className='center painting-container'>
+        <div className='painting'>
+          <img src={tulip} style={{width: '800px'}} alt='Sample'/>
+        </div>
+      </ div>
     );
   }
 }
