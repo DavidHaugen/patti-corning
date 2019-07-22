9fc205583c2a2d2fb1d721850ac1e6a47c9346f6
David Haugen <haugen.dj@gmail.com>
2019-07-22 16:34:39 -0500

    block out major components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 import Nav from './Nav/Nav';
+import Home from './Home/Home';
+import Contact from './Contact/Contact';
+import NotFound from './NotFound/NotFound';
+import Paintings from './Paintings/Paintings';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <>
       <Nav />
       <main>
-
+        <Switch>
+          <Route
+            exact path={'/'}
+            component={Home}
+          />
+          <Route
+            path={'/paintings'}
+            component={Paintings}
+          />
+          <Route
+            path={'/contact'}
+            component={Contact}
+          />
+          <Route
+            component={NotFound}
+          />
+        </Switch>
+        <Footer />
       </main>
     </>
   );
