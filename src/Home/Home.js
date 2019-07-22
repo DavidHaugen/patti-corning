7b3aadc24c163a7dbeef33477376e4b46fb7c285
David Haugen <haugen.dj@gmail.com>
2019-07-22 17:40:33 -0500

    add functions and fontawesome buttons for cycling through images

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,13 +4,36 @@ import paintingFlowers from '../images/painting-flowers.jpeg';
 import tulip from '../images/tulip.jpeg';
 import './Home.css';
 
+
 export default class Home extends Component{
+  state = {
+    images: [hibiscus, paintingFlowers, tulip],
+    currentImage: 0
+  }
+  nextImage = () => {
+    if(this.state.currentImage < this.state.images.length -1){
+      this.setState({currentImage: this.state.currentImage +1});
+    } else{
+      this.setState({currentImage: 0});
+    }
+  }
+
+  previousImage = () => {
+    if(this.state.currentImage > 0){
+      this.setState({currentImage: this.state.currentImage -1});
+    } else {
+      this.setState({currentImage: this.state.images.length - 1});
+    }
+  }
+
   render(){
     return (
-      <div className='center painting-container'>
+      <div className='center painting-container flex'>
+        <i className="fas fa-arrow-left arrow-button" onClick={this.previousImage}></i>
         <div className='painting'>
-          <img src={tulip} style={{width: '800px'}} alt='Sample'/>
+          <img src={this.state.images[this.state.currentImage]} style={{width: '800px'}} alt='Sample'/>
         </div>
+        <i className="fas fa-arrow-right arrow-button" onClick={this.nextImage}></i>
       </ div>
     );
   }
